ff8945825857136a9b3a4c2807d1df32269fb609
liutao <465275024@qq.com>
2020-06-16 19:15:37 +0800

    Button

---
+++ src/package/button/Button.vue
@@ -0,0 +1,87 @@
+<template>
+    <button @click="handlerClick"
+            class="el-button"
+    :class="[
+        colorType?'el-button--'+colorType:'',
+        size?'el-button--'+size:'',
+        {
+        'is-round':round
+        }
+    ]">
+        按钮
+    </button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    colorType: {
+      type: String,
+      default: 'black'
+    },
+    round: Boolean,
+    size: {
+      type: String,
+      default: 'normal'
+    }
+  },
+  methods: {
+    handlerClick (evt) {
+      this.$emit('click', evt)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+    .el-button--red {
+        color: black;
+    }
+
+    .el-button--red {
+        color: red;
+    }
+
+    .el-button--blue {
+        color: blue;
+    }
+
+    .el-button--max {
+        width: 100px;
+        height: 56px;
+    }
+    .el-button{
+        display: inline-block;
+        line-height: 1;
+        white-space: nowrap;
+        cursor: pointer;
+        background: #fff;
+        border: 1px solid #dcdfe6;
+        color: #606266;
+        -webkit-appearance: none;
+        text-align: center;
+        box-sizing: border-box;
+        outline: none;
+        margin: 0;
+        transition: .1s;
+        font-weight: 500;
+        -moz-user-select: none;
+        -webkit-user-select: none;
+        -ms-user-select: none;
+        padding: 12px 20px;
+        font-size: 14px;
+        border-radius: 4px;
+    }
+    .el-button--normal {
+        width: 80px;
+        height: 45px;
+    }
+    .el-button--min {
+        width: 60px;
+        height: 33px;
+    }
+    .is-round {
+        border-radius: 10px;
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Button color-type="blue" size="mini"></Button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Button from '@/package/button/Button'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  components: { Button }
 }
 </script>
